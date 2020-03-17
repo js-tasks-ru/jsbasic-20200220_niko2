011e3f6f580fe786b06735c7bc50dab0bf9b865f
Ivan Kubik <kubik.ivan92@gmail.com>
2020-03-18 01:35:17 +0300

    2-module-completed

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,15 @@
 let calculator = {
-  // ваш код
+	read() {
+		this.a = 3;
+		this.b = 5;
+	},
+	sum() {
+		return this.a + this.b;
+	},
+	mul() {
+		return this.a * this.b;
+	},
 };
-
+calculator.read(3, 5);
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
