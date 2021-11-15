c66cab3babf9c3a72eaeb011dbdff35c13915f52
Иван Кубик <ivankubik@Ivans-MacBook-Pro.local>
2021-11-15 20:17:16 +0300

    complete 5 module

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,38 @@
  * @param {Element} table
  */
 function highlight(table) {
+  let tbody = table.querySelector('tbody');
+  let tr = tbody.querySelectorAll('tr');
+
+  let i = 1;
+
+  for (let elem of tr) {
+    let available = elem.lastElementChild.getAttribute('data-available');
+
+    if (available === 'true') {
+      elem.classList.add('available');
+    }
+    if (available === 'false') {
+      elem.classList.add('unavailable');
+    }
+    if (!available) {
+      elem.setAttribute('hidden', '');
+    }
+
+    let male = table.rows[i].cells[2].innerHTML;
+
+    if (male === 'm') {
+      elem.classList.add('male');
+    }
+    if (male === 'f') {
+      elem.classList.add('female');
+    }
+
+    let size = table.rows[i].cells[1].innerHTML;
+
+    if (size <= 18) {
+      elem.style.textDecoration = 'line-through';
+    }
+    i++;
+  }
 }
